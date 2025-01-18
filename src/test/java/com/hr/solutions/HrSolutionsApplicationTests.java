319232a93a6a0a2349cf2cb99acbcad56e4313b4
Patryk Krzyżak <p.krzyzak1986@gmail.com>
2025-01-18 12:58:07 +0100

    Initial commit

---
+++ src/test/java/com/hr/solutions/HrSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr.solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
